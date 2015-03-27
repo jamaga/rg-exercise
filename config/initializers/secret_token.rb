4f32368856128d539ed7a4722d3157cc6a069294
jamaga <hipo.beaver@gmail.com>
2015-03-27 20:25:50 +0100

    1st com

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Diary::Application.config.secret_key_base = 'abf9486020c97ff130e620e9f4192c5b6bf3aab439d93055c5d7b949773bdb183a7abed83e8a064f4f7e8c619cdc9d8772d7a9d425640404d547d70f8566376e'
